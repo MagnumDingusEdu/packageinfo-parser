d8e3eeff25219a70996986d982b275a3a3151d19
Vividh Mariya <vmariya_be19@thapar.edu>
2021-03-28 14:40:41 +0530

    Added python script

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import subprocess
+from typing import List
 
 # Run the script
 # Does not log output for now
 rc = subprocess.call("./get_packages.sh", shell=True)
+
+with open('arch_list.txt') as f:
+    archlinux_repo_list: List[str] = f.readlines()
+
+with open('fedora_list.txt') as f:
+    fedora_repo_list: List[str] = f.readlines()
+
+with open('ubuntu_list.txt') as f:
+    ubuntu_repo_list: List[str] = f.readlines()
+
+
+class Package:
+    name: str
+    version: str
+    repository: str
+    distribution: str
+    architecture: str
+
+
